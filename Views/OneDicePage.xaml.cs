28986a7f731990c5a4fc161068d99d3433cf48f8
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2023-09-11 01:48:36 +0300

    Implement One Dice Page

--- Views/OneDicePage.xaml.cs
+++ Views/OneDicePage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace Dice_App.Views;
 
 public partial class OneDicePage : ContentPage
@@ -6,4 +8,15 @@ public partial class OneDicePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ImageButtonReturn_Clicked(object sender, EventArgs e)
+    {
+		Application.Current.MainPage = new MainPage();
+    }
+
+    private void BtnRoll_Clicked(object sender, EventArgs e)
+    {
+        int randomDiceFace = new Random().Next(1, 7);
+        imgDice.Source = $"dice_face_{randomDiceFace}.png";
+    }
 }
