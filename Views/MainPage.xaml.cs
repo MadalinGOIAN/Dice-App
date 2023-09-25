a9aee05d7f9c84630dc8b08a943040b643b2510d
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2023-09-25 23:18:47 +0300

    Simplify Layout

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -5,21 +5,6 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
         InitializeComponent();
-
-        InitializeButton1Dice();
-        InitializeButton2Dices();
-    }
-
-    private void InitializeButton1Dice()
-    {
-        btn1Dice.WidthRequest = DeviceDisplay.MainDisplayInfo.Width * 0.2;
-        btn1Dice.HeightRequest = DeviceDisplay.MainDisplayInfo.Height * 0.04;
-    }
-
-    private void InitializeButton2Dices() 
-    {
-        btn2Dices.WidthRequest = DeviceDisplay.MainDisplayInfo.Width * 0.2;
-        btn2Dices.HeightRequest = DeviceDisplay.MainDisplayInfo.Height * 0.04;
     }
 
     private void Btn1Dice_Clicked(object sender, EventArgs e)
